5616b4a1b2eb89ae7cfc5fbb67551c8183a8768f
Byxs20 <97766819@qq.com>
2023-02-17 21:24:04 +0800

    modify tools.py

--- usb/tools.py
+++ usb/tools.py
@@ -1,10 +1,10 @@
 import os
-import pyshark
 import subprocess
 
 
 maximum = 0.8 # 流量小于0.8MB才会自动读取流量大小
 filters = ["usb.capdata", "usbhid.data"]
+CACHE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache")
 
 def get_input():
     while True:
@@ -13,22 +13,24 @@ def get_input():
         elif tmp == "":
             return None
 
+def get_length(file_path, filter):
+    command = f'tshark -r {file_path} -Y {filter} -T fields -e {filter}'
+    res = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    with open(f"{os.path.join(CACHE_PATH, filter)}.txt", "w") as f:
+        data = res.stdout.read().decode("gbk").replace("\r\n", "\n")
+        f.write(data)
+    return len(data.splitlines())
+
 def get_select(file_path):
-    select = 1
-    if (size:= round(os.path.getsize(file_path) / (1024 * 1024), 2)) < maximum:
-
-        lengths = []
-        for filter in filters:
-            cap = pyshark.FileCapture(file_path, display_filter=filter)
-            cap.load_packets()
-            lengths.append(len(cap))
-
-        if lengths[0] >= lengths[1]:
-            print(f"[-]: usb.size: {size}(MB), usb.capdata: {lengths[0]}, usbhid.data: {lengths[1]}, 默认读取: {filters[0]}")
-            select = 1
-        else:
-            print(f"[-]: usb.size: {size}, usb.capdata: {lengths[0]}, usbhid.data: {lengths[1]}, 默认读取: {filters[1]}")
-            select = 2
+    size = round(os.path.getsize(file_path) / (1024 * 1024), 2)
+    lengths = [get_length(file_path, filter) for filter in filters]
+    
+    if lengths[0] >= lengths[1]:
+        print(f"[-]: usb.size: {size}(MB), usb.capdata: {lengths[0]}, usbhid.data: {lengths[1]}, 默认读取: {filters[0]}")
+        select = 1
+    else:
+        print(f"[-]: usb.size: {size}(MB), usb.capdata: {lengths[0]}, usbhid.data: {lengths[1]}, 默认读取: {filters[1]}")
+        select = 2
 
     if (tmp := get_input()) is not None:
         select = tmp
@@ -36,9 +38,9 @@ def get_select(file_path):
 
 def get_data(file_path):
     select = get_select(file_path)
-    command = f'tshark -r {file_path} -Y {filters[select-1]} -T fields -e {filters[select-1]}'
-    res = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return res.stdout.read().decode("gbk").splitlines()
+    with open(f"{os.path.join(CACHE_PATH, filters[select - 1])}.txt", "r") as f:
+        data = f.read().splitlines()
+    return data
 
 if __name__ == '__main__':
-    get_data("usb.pcapng")
+    print(get_data("usb.pcapng"))
