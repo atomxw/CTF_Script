6bcc07626a1f0c458bc746fdb0b7957f6e77c530
Byxs20 <97766819@qq.com>
2023-03-15 21:33:15 +0800

    update main.py

--- FIX_BMP/main.py
+++ FIX_BMP/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import math
 import argparse
 import numpy as np
@@ -13,33 +14,42 @@ args  = parser.parse_args()
 SAVE_DIR = os.getcwd()
 
 def save_img(data, width=None, height=None, sqrt_num=None):
-    with open(os.path.join(SAVE_DIR, "width.bmp"), "wb") as f:
+    with open(os.path.join(SAVE_DIR, "fix_width.bmp"), "wb") as f:
         f.write(data[:0x12] + width.to_bytes(4, byteorder="little", signed=False) + data[0x16:])
 
-    with open(os.path.join(SAVE_DIR, "height.bmp"), "wb") as f:
+    with open(os.path.join(SAVE_DIR, "fix_height.bmp"), "wb") as f:
         f.write(data[:0x16] + height.to_bytes(4, byteorder="little", signed=False) + data[0x1a:])
 
-    with open(os.path.join(SAVE_DIR, "sqrt.bmp"), "wb") as f:
+    with open(os.path.join(SAVE_DIR, "fix_sqrt.bmp"), "wb") as f:
         f.write(data[:0x12] + sqrt_num.to_bytes(4, byteorder="little", signed=False) * 2 + data[0x1a:])
+
+def get_pixels_size(data):
+    bfSize = int.from_bytes(data[0x2:0x2+4], byteorder="little", signed=False)
+    bfOffBits = int.from_bytes(data[0xa:0xa+4], byteorder="little", signed=False)
+    biBitCount = int.from_bytes(data[0x1c:0x1c+2], byteorder="little", signed=False)
+    channel = biBitCount // 8
+    # 由于宽高都会被修改，所以我计算出来的Padding_size也不是正确的，没有意义
+    # padding_size = (4 - col * channel % 4) * row if col * channel % 4 != 0 else 0
+    # pixels_size = (bfSize - bfOffBits - padding_size) // channel
+    return (bfSize - bfOffBits) // channel
 
 
 if __name__ == '__main__':
-    file_path = os.path.abspath(args.f) 
-
-    img = Image.open(file_path).convert('RGB')
-    img = np.array(img, dtype=np.uint8)
-    assert img.shape[-1] == 3
-
-    size = os.path.getsize(file_path)
-    row, col = img.shape[:2]
-    width = (size - 53) // 3 // row
-    height = (size - 53) // 3 // col
-    sqrt_num = int(math.sqrt((size - 53) // 3))
-
-    print(f"1.宽度可能为: {width}")
-    print(f"2.高度可能为: {height}")
-    print(f"3.宽度和高度可能为: {sqrt_num}")
+    file_path = os.path.abspath(args.f)
 
     with open(file_path, "rb") as f:
         data = f.read()
+
+    img = np.array(Image.open(file_path).convert('RGB'), dtype=np.uint8)
+    row, col = img.shape[:2]
+    pixels_size = get_pixels_size(data)
+
+    width, height = pixels_size // row, pixels_size // col
+    sqrt_num = int(math.sqrt((pixels_size)))
     save_img(data, width=width, height=height, sqrt_num=sqrt_num)
+
+    print("温馨提示：由于填充字节的问题，所以可能会偏差几个像素!")
+    print(f"1.修复宽度: {width}")
+    print(f"2.修复高度: {height}")
+    print(f"3.修复宽度高度为: {sqrt_num}")
+    time.sleep(1)
