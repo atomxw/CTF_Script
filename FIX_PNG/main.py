cdb37c276701a270c6fcfc9a1334c77251c8ffaa
Byxs20 <97766819@qq.com>
2023-03-19 00:29:32 +0800

    update main.py

--- FIX_PNG/main.py
+++ FIX_PNG/main.py
@@ -11,14 +11,9 @@ parser.add_argument("-f", type=str, default=None, required=True,
                     help="输入同级目录下图片的名称")
 args  = parser.parse_args()
 
-png_name = args.f.split("\\")[-1]
-png_dir = os.path.abspath(args.f)
-base_dir = os.path.dirname(png_dir)
-
-bin_data = open(png_dir, 'rb').read()
-crc32key = zlib.crc32(bin_data[12:29]) # 计算crc
-original_crc32 = int(bin_data[29:33].hex(), 16) # 原始crc
-
+PNG_NAME = args.f.split("\\")[-1]
+PNG_DIR = os.path.abspath(args.f)
+BASE_DIR = os.path.dirname(PNG_DIR)
 
 # 老铜匠
 def crack_no_inter(bt_ch, width, data, tmppath, chuncks):
@@ -92,25 +87,48 @@ def pngbaoli_def(filename):
     bt_ch_my = [(6, 4, 8)]  # 比较常用的6,4,8
     crack_no_inter(bt_ch_my, 10001, data, tmppath, chuncks)
 
+def calculation_crc32(width, height):
+    return zlib.crc32(data[12:16] + struct.pack('>i', width) + struct.pack('>i', height) + data[24:29])
+
+def save_png(width, height):
+    with open(os.path.join(BASE_DIR, f"fix_{PNG_NAME}"), "wb") as f:
+        print(f"[-] 宽度: {width}, hex: {hex(width)}")
+        print(f"[-] 高度: {height}, hex: {hex(height)}")
+        f.write(data[:16] + struct.pack(">i", width) + struct.pack(">i", height) + data[24:])
+        print(f"[-] CRC32: {hex(original_crc32)}, 已经为您保存到运行目录中!")
 
 if __name__ == '__main__':
-    if os.path.splitext(png_name)[1] == ".png":
-        if crc32key == original_crc32: # 计算crc对比原始crc
-            print("[-] 计算CRC32, 宽高没有问题, 开始尝试暴力破解!(感谢老铜匠)")
-            pngbaoli_def(args.f)
-        else:
-            for width, height in itertools.product(range(0x1FFF), range(0x1FFF)): # 理论上0x FF FF FF FF，但考虑到屏幕实际/cpu，0x1FFF就差不多了，也就是8191宽度和高度
-                data = bin_data[12:16] + struct.pack('>i', width) + struct.pack('>i', height) + bin_data[24:29]
-                crc32 = zlib.crc32(data)
-                
-                if(crc32 == original_crc32): # 计算当图片大小为width:height时的CRC校验值，与图片中的CRC比较，当相同，则图片大小已经确定
-                    with open(os.path.join(base_dir, f"fix_{png_name}"), "wb") as f:
-                        print(f"[-] CRC32: {hex(original_crc32)}")
-                        print(f"[-] 宽度: {width}, hex: {hex(width)}")
-                        print(f"[-] 高度: {height}, hex: {hex(height)}")
-                        f.write(bin_data[:16] + struct.pack(">i", width) + struct.pack(">i", height) + bin_data[24:])
-                        print("[-] 已经为您保存到运行目录中!")
-                        break
-    else:
+    data = open(PNG_DIR, 'rb').read()
+    crc32key = zlib.crc32(data[12:29]) # 计算crc
+    original_width, original_height = int.from_bytes(data[0x10:0x10+4], byteorder="big", signed=False), int.from_bytes(data[0x14:0x14+4], byteorder="big", signed=False)
+    original_crc32 = int(data[29:33].hex(), 16) # 原始crc
+
+    if os.path.splitext(PNG_NAME)[1] != ".png":
         print("[-] 您的文件后缀名不为PNG!")
-    time.sleep(1)
+        exit(-1)
+
+    if crc32key == original_crc32: # 计算crc对比原始crc
+        print("[-] 计算CRC32, 宽高没有问题, 开始尝试暴力破解!(感谢老铜匠)")
+        pngbaoli_def(args.f)
+        time.sleep(1)
+    else:
+        print("[-] 爆破高度中...")
+        for height in range(0x1FFF):
+            if calculation_crc32(original_width, height) == original_crc32:
+                save_png(original_width, height)
+                time.sleep(1)
+                exit()
+
+        print("[-] 爆破宽度中...")
+        for width in range(0x1FFF):
+            if calculation_crc32(width, original_height) == original_crc32:
+                save_png(width, original_height)
+                time.sleep(1)
+                exit()
+
+        print("[-] 爆破宽度和高度中...")
+        for width, height in itertools.product(range(0x1FFF), range(0x1FFF)): # 理论上0x FF FF FF FF，但考虑到屏幕实际/cpu，0x1FFF就差不多了，也就是8191宽度和高度
+            if calculation_crc32(width, height) == original_crc32: # 计算当图片大小为width:height时的CRC校验值，与图片中的CRC比较，当相同，则图片大小已经确定
+                save_png(width, height)
+                time.sleep(1)
+                exit()
