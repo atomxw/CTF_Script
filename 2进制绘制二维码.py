5b6ee8f6051208f4901668422aefddf669fad94e
Byxs20 <97766819@qq.com>
2023-05-25 12:25:51 +0800

    更新优化代码

--- 2进制绘制二维码.py
+++ 2进制绘制二维码.py
@@ -1,53 +1,55 @@
 import os
 import cv2
 import time
+import shutil
 import argparse
-import itertools
 import numpy as np
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', type=str, default=None, required=True,
                     help='输入文件名称')
-parser.add_argument('-size', type=int, default=5,
-                    help='图片放大倍数(默认5倍)')
+parser.add_argument('-size', type=int, default=1,
+                    help='图片放大倍数(默认1倍)')
 args = parser.parse_args()
 
 file_path = os.path.join(args.f)
 
-if not os.path.exists("./out"):
-    os.mkdir("./out")
+saveDir = "./out"
+if os.path.exists(saveDir):
+    shutil.rmtree(saveDir, ignore_errors=True)
+os.makedirs(saveDir)
 
-# read binary txt
-with open(file_path, "r") as f:
-    data = f.read().strip()
+def crete_img(bin_str):
+    np_arr = np.frombuffer(bin_str.encode(), dtype=np.uint8) - ord('0') # 使用 - ord('0') 将 ASCII 码值为 48 的字符 '0' 转换为整数 0，将 ASCII 码值为 49 的字符 '1' 转换为整数 1。
 
-def draw_QR(img, reverse=False):
-    for i, v in enumerate(data[:row*col]):
-        right_bottom_point = (left_top_point[i][0] + size, left_top_point[i][1] + size)
-        if not reverse:
-            cv2.rectangle(img, left_top_point[i], right_bottom_point, color=(
-                255, 255, 255), thickness=-1) if v == "0" else cv2.rectangle(img, left_top_point[i], right_bottom_point, color=(0, 0, 0), thickness=-1)
-        else:
-            cv2.rectangle(img, left_top_point[i], right_bottom_point, color=(0, 0, 0), thickness=-1) if v == "0" else cv2.rectangle(
-                img, left_top_point[i], right_bottom_point, color=(255, 255, 255), thickness=-1)
-    return img
+    img = np_arr.copy()
+    img[img==1] = 255
+
+    invert_img = np_arr.copy()
+    invert_img[img==0] = 255
+    invert_img[img==1] = 0
+    return img, invert_img
 
 if __name__ == '__main__':
-    # 计算宽高
-    dic = {X: int(len(data) / X) for X in range(1, len(data)) if len(data) % X == 0}
     size = args.size
+    
+    with open(file_path, "r") as f:
+        bin_str = f.read().strip()
 
+    img, invert_img = crete_img(bin_str)
+    
+    dic = {X: int(len(bin_str) / X) for X in range(1, len(bin_str)) if len(bin_str) % X == 0}
     for row, col in dic.items():
-        img1, img2 = np.zeros((row * size, col * size, 1)), np.zeros((row * size, col * size, 1))
+        save_img = img.reshape(row, col)
+        save_invert_img = cv2.resize(save_img, None, fx=size, fy=size, interpolation=cv2.INTER_AREA)
 
-        left_top_point = []
-        for i, j in itertools.product(range(0, row * size, size), range(0, col * size, size)):
-            left_top = (j, i)
-            left_top_point.append(left_top)
-
-        cv2.imwrite(f"./out/{col}_{row}.png", draw_QR(img1, reverse=False))
-        cv2.imwrite(f"./out/{col}_{row}_reverse.png", draw_QR(img2, reverse=True))
+        if size > 1:
+            save_img = cv2.resize(save_img, None, fx=size, fy=size, interpolation=cv2.INTER_AREA)
+            save_invert_img = cv2.resize(save_invert_img, None, fx=size, fy=size, interpolation=cv2.INTER_AREA)
+        
+        cv2.imwrite(f"./{saveDir}/{col}_{row}.png", save_img)
+        cv2.imwrite(f"./{saveDir}/{col}_{row}_inverse.png", save_invert_img)
         print(f"[-] 宽度:{col:6} 高度:{row:6}, 已保存在运行目录out中...")
     print("[-] 已经遍历完所有情况, 即将自动关闭!")
     time.sleep(0.5)
