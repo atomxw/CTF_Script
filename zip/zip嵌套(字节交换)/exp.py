8cecb268c5b3ce532b5c9d9958f185d62c2efc77
Byxs20 <97766819@qq.com>
2023-06-20 11:54:46 +0800

    支持中文

--- zip/zip嵌套(字节交换)/exp.py
+++ zip/zip嵌套(字节交换)/exp.py
@@ -10,12 +10,13 @@ all_files_processed = False  # 初始化标志变量
 while True:
     with zipfile.ZipFile(io.BytesIO(data), "r") as zf:
         for i in zf.filelist:
+            fileName = i.filename.encode("cp437").decode("gbk")
             if zipfile.is_zipfile(io.BytesIO(zf.read(i.filename)[::-1])):
                 data = zf.read(i.filename)[::-1]
                 print(i.filename)
             else:
                 all_files_processed = True
-                with open(i.filename, "wb") as f:
+                with open(fileName, "wb") as f:
                     f.write(zf.read(i.filename))
 
         if all_files_processed:
