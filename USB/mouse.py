135f8328db2f55eca302c7579d0504806cb20488
Byxs20 <97766819@qq.com>
2023-08-21 21:45:28 +0800

    修改为4种模式

--- USB/mouse.py
+++ USB/mouse.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", type=str, default=None, required=True,
                     help="输入同级目录下的名称")
 parser.add_argument("-show", type=int, default=None, required=False,
-                    help="1.LEFT, 2.RIGHT, 3.ALL, 4.MOVE_LEFT, 5.MOVE_RIGHT, 6.MOVE_ALL")
+                    help="单独显示某个(1.LEFT/MOVE_LEFT, 2.RIGHT/MOVE_RIGHT, 3.ALL/MOVE_ALL)")
 args  = parser.parse_args()
 
 TITLE = ["LEFT", "RIGHT", "ALL", "MOVE_LEFT", "MOVE_RIGHT", "MOVE_ALL"]
@@ -23,7 +23,7 @@ def read_file():
         data = tools.get_data(FILE_PATH)
     return data
 
-def get_pos(mode):
+def get_pos(mode, btn_mode):
     posx, posy = 0, 0
     pos_left, pos_right, pos_all = [], [], []
     for line in data:
@@ -42,7 +42,7 @@ def get_pos(mode):
         posx += x
         posy += y
         # 1 for left , 2 for right , 0 for nothing
-        btn_flag = int(line[:2], 16)
+        btn_flag = int(line[2:4], 16) if btn_mode else int(line[:2], 16)
         if btn_flag == 1:  # 1 代表左键，2代表右键
             pos_left.append((posx, -posy))
             pos_all.append((posx, -posy))
@@ -55,9 +55,12 @@ def plot_point(arr, axes, move=False):
     if arr.size != 0:
         axes.plot(arr[:, 0], arr[:, 1], c='purple') if move else axes.scatter(arr[:, 0], arr[:, 1], s=10, c='purple', marker="x")
 
-def printInfo(mode, info):
-    all_pos = get_pos(mode)
-
+def printInfo(mode, btn_mode, info):
+    all_pos = get_pos(mode, btn_mode)
+    if len(list(filter(lambda x: x.size == 0, all_pos))) == 3:
+        print("当前模式为空, 已经自动帮您跳过!")
+        return
+    
     _, axes = plt.subplots(2, 3, figsize=(15, 8))
     for i in range(len(axes)):
         for j in range(len(axes[i])):
@@ -68,17 +71,18 @@ def printInfo(mode, info):
                 plot_point(all_pos[count%3], ax)
             else:
                 plot_point(all_pos[count%3], ax, move=True)
-
+    
     print(info)
     plt.tight_layout()
     plt.show()
     
-def showInfo(mode, info):
-    all_pos = get_pos(mode)
+def showInfo(mode, btn_mode, info):
+    all_pos = get_pos(mode, btn_mode)
     
     if args.show in [1, 2, 3, 4, 5, 6]:
         arr = all_pos[args.show%3-1]
         if arr.size == 0:
+            print("当前模式为空, 已经自动帮您跳过!")
             return
         
         if args.show < 3 :
@@ -94,8 +98,12 @@ if __name__ == '__main__':
     data = read_file()
     
     if args.show:
-        showInfo(False, "模式1绘制完毕!")
-        showInfo(True, "模式2绘制完毕!")
+        showInfo(False, False, "模式1绘制完毕!")
+        showInfo(True, False, "模式2绘制完毕!")
+        showInfo(False, True, "模式3绘制完毕!")
+        showInfo(True, True, "模式4绘制完毕!")
     else:
-        printInfo(False, "模式1绘制完毕!")
-        printInfo(True, "模式2绘制完毕!")
+        printInfo(False, False, "模式1绘制完毕!")
+        printInfo(True, False, "模式2绘制完毕!")
+        printInfo(False, True, "模式3绘制完毕!")
+        printInfo(True, True, "模式4绘制完毕!")
